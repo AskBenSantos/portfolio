9f4ec8948a9b14dd489063ccf138d65c7bf3ad57
Benedict Okeh <benedictokeh5@gmail.com>
2026-08-25 11:17:36 +0100

    update on the img

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const themeToggle = document.querySelector("#theme-toggle");
 themeToggle.addEventListener("click", function(){
 document.body.classList.toggle("light-mode");
-themeToggle.textContent = 'Dark Mode'
+
+if (document.body.classList.contains("light-mode")){
+  themeToggle.textContent = "Dark Mode";
+} else{
+  themeToggle.textContent = "Light Mode";
+}
 });
